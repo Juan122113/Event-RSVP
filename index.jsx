82cf6170814833924eecfd91115f672ed5695d13
Juan122113 <ammiel89@hotmail.com>
2025-11-13 21:25:32 -0300

    Initial commit

---
+++ index.jsx
@@ -0,0 +1,15 @@
+const { useState } = React;
+
+export function EventRSVPForm() {
+ 
+  return(
+    <form action="">
+      <input type="text" />
+      <input type="text" />
+      <input type="email" />
+      <input type="number" />
+      <input type="checkbox" />
+    </form>
+  );
+
+}
